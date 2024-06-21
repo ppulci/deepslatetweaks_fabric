7e80db543c1afb4c967ab150f146de626001db3a
Peter Pulcini <peter.pulcini89@gmail.com>
2024-06-21 00:03:08 -0400

    Updated 1.20.5

---
+++ src/client/java/com/syco/deepslatetweaksclient/DeepslateTweaksClient.java
@@ -0,0 +1,10 @@
+package com.syco.deepslatetweaksclient;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeepslateTweaksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
